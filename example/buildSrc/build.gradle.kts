97f6765679dea6c689520395eae2394973f9350e
Karol Czeryna <karol.czeryna@gmail.com>
2021-03-04 21:02:31 +0100

    Fix gradle config in example module

--- example/buildSrc/build.gradle.kts
+++ example/buildSrc/build.gradle.kts
@@ -14,21 +14,5 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
-    implementation("io.github.eeqk.gudd:io.github.eeqk.gudd.gradle.plugin:0.0.0")
-}
-
-
-repositories {
-    maven {
-        url = uri("https://maven.pkg.github.com/eEQK/gudd")
-        credentials {
-            username = System.getenv("GITHUB_USER")
-            password = System.getenv("GITHUB_TOKEN") // with read:packages permission
-        }
-    }
-
-}
-
-dependencies {
     implementation("io.github.eeqk.gudd:io.github.eeqk.gudd.gradle.plugin:1.0.0")
 }
